9c747578288e9fac4e7805f38472280f2ee5c995
Fernando Angolar <fernandoangolar581@gmail.com>
2025-02-25 21:53:33 +0100

    :recycle: refactor: O metodo de busca todos os livros, foi alterado para logica de paginação dos livros

--- src/main/java/ao/com/angotech/service/LivroService.java
+++ src/main/java/ao/com/angotech/service/LivroService.java
@@ -3,12 +3,13 @@ package ao.com.angotech.service;
 import ao.com.angotech.dto.LivroCreateDTO;
 import ao.com.angotech.dto.LivroResponseDTO;
 import ao.com.angotech.dto.LivroUpdateDTO;
+import org.springframework.data.domain.Page;
 
 public interface LivroService {
 
     LivroResponseDTO cadastrarLivros(LivroCreateDTO dto);
     LivroResponseDTO atualizarLivros(Long id, LivroUpdateDTO dto);
     LivroResponseDTO buscarLivro(Long id);
-    LivroResponseDTO buscarTodosOsLivros();
+    Page<LivroResponseDTO> buscarTodosOsLivros(String titulo, String autor, int page, int size, String sortBy, String direction);
     void excluirLivro(Long id);
 }
